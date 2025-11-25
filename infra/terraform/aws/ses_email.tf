7999a3a3a1eb13fe475befecf3e205b46fc851fb
Luis Gonzalez <luisal.gonzaleze@gmail.com>
2025-11-25 15:12:33 -0600

    feat: terraform implementation

--- infra/terraform/aws/ses_email.tf
+++ infra/terraform/aws/ses_email.tf
@@ -2,13 +2,13 @@
 
 # TODO: Descomentar cuando se tenga acceso a AWS
 
-# resource "aws_ses_email_identity" "sender" {
-#   email = var.ses_sender_email
-# }
+resource "aws_ses_email_identity" "sender" {
+  email = var.ses_sender_email
+}
 
-# resource "aws_ses_configuration_set" "reparaya" {
-#   name = "${var.project_name}-${var.environment}"
-# }
+resource "aws_ses_configuration_set" "reparaya" {
+  name = "${var.project_name}-${var.environment}"
+}
 
 # TODO: Configurar SNS topic para bounce/complaint notifications (opcional)
 # TODO: Mover de Sandbox a Production (requiere solicitud a AWS)
--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -3,27 +3,39 @@
 
 # TODO: Descomentar cuando se creen los recursos
 
-# output "s3_media_bucket_name" {
-#   description = "Nombre del bucket S3 para medios"
-#   value       = aws_s3_bucket.media.id
-# }
+output "s3_media_bucket_name" {
+  description = "Nombre del bucket S3 para medios"
+  value       = module.aws_resources.s3_media_bucket_name
+}
 
-# output "s3_media_bucket_arn" {
-#   description = "ARN del bucket S3 para medios"
-#   value       = aws_s3_bucket.media.arn
-# }
+output "s3_media_bucket_arn" {
+  description = "ARN del bucket S3 para medios"
+  value       = module.aws_resources.s3_media_bucket_arn
+}
 
-# output "ses_sender_email" {
-#   description = "Email configurado en SES"
-#   value       = var.ses_sender_email
-# }
+output "ses_sender_email" {
+  description = "Email configurado en SES"
+  value       = var.ses_sender_email
+}
 
-# output "location_place_index_name" {
-#   description = "Nombre del Place Index de Amazon Location"
-#   value       = aws_location_place_index.places.index_name
-# }
+output "location_place_index_name" {
+  description = "Nombre del Place Index de Amazon Location"
+  value       = module.aws_resources.location_place_index_name
+}
 
-# output "location_route_calculator_name" {
-#   description = "Nombre del Route Calculator de Amazon Location"
-#   value       = aws_location_route_calculator.routes.calculator_name
-# }
+output "location_route_calculator_name" {
+  description = "Nombre del Route Calculator de Amazon Location"
+  value       = module.aws_resources.location_route_calculator_name
+}
+
+output "app_access_key_id" {
+  description = "Access Key ID para la aplicación"
+  value       = module.aws_resources.app_access_key_id
+  sensitive   = true
+}
+
+output "app_secret_access_key" {
+  description = "Secret Access Key para la aplicación"
+  value       = module.aws_resources.app_secret_access_key
+  sensitive   = true
+}
